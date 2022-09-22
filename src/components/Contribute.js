d6983f5314faf42464359d61b419e0a65ab1b5d6
JunickMarco <workjunick@gmail.com>
2022-09-22 14:37:31 +0800

    HandleOnsubmit POST

--- src/components/Contribute.js
+++ src/components/Contribute.js
@@ -1,24 +1,26 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Contribute = () => {
 
     const handleSubmit = (event) =>{
         event.preventDefault();
 
+        console.log("POST! Added Contribution.")
         // const post = { 
-        //     title, 
-        //     body, 
+        //     text, 
+        //     result, 
         //     author_id: userId,
         //     author: props.user.username,
         //     category, 
         //     isAnonPost 
         // };
 
-        // setIsPending(true);
-        
-        // fetch("http://localhost:3002/posts", {
+        // fetch("http://localhost:3002/infos", {
         //     method: "POST",
         //     headers: { "Content-Type": "application/json" },
         //     body: JSON.stringify(post)
@@ -31,32 +33,39 @@ const Contribute = () => {
 
     return (
         <div className="form-container">
-            <h2>Contribute!</h2>
-            <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label htmlFor="inputData">Data</Form.Label>
-                    <Form.Control
-                        type="text"
-                        id="inputData"
-                        aria-describedby="dataHelpBlock"
-                    />
-                    <Form.Text id="dataHelpBlock" muted>
-                        Enter a phrase you want to classify as fake or not.
-                    </Form.Text>
-                </Form.Group>
+            <Container>
+                <Row className="justify-content-md-center">
+                    <Col sm={4}>
+                        <h2>Contribute!</h2>
+                        <Form onSubmit={handleSubmit}>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label htmlFor="inputData">Data</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    id="inputData"
+                                    aria-describedby="dataHelpBlock"
+                                />
+                                <Form.Text id="dataHelpBlock" muted>
+                                    Enter a phrase you want to classify as fake or not.
+                                </Form.Text>
+                            </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check 
-                        type="switch"
-                        id="custom-switch"
-                        label="Fake"
-                    />
-                </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                                <Form.Check 
+                                    type="switch"
+                                    id="custom-switch"
+                                    label="Fake"
+                                />
+                            </Form.Group>
 
-                <Button variant="primary" type="submit">
-                    Submit Contribution
-                </Button>
-            </Form>
+                            <Button variant="primary" type="submit">
+                                Submit Contribution
+                            </Button>
+                        </Form>
+                    </Col>
+                </Row>
+                
+            </Container>
         </div>
     );
 };
