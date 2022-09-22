9507f097a753b58048c0d6640ff6c887db0cc63e
JunickMarco <workjunick@gmail.com>
2022-09-23 06:33:58 +0800

    Added Fetch in Home to retrieve result of /check.

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,21 +2,38 @@ import { useState } from 'react';
 import { Card, Col, Container, Row } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import check from "../assets/verified.svg";
-import nodata from "../assets/no-data.svg";
-import uncertain from "../assets/no-data.svg";
 import Status from './Status';
 
 function Home() {
-  const [ isFake, setIsFake] = useState();
+  const [ topic, setTopic ] = useState("");
   const data = {result: 48, status: "UNCERTAIN"};
 
   const [status, setStatus] = useState(data.status);
 
+  const handleOnChange = (event) => {
+    setTopic(event.target.value);
+  }
 
   function handleSubmit(){
-    setIsFake(Math.round(Math.random() * 1));
-    console.log("Fake", isFake)
+    
+    const topicObj = {
+      text: topic
+    };
+
+    fetch("http://localhost:3002/check", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(topicObj)
+    })
+    .then((response) => response.json())
+    .then((result) => {
+        console.log("Topic requested.");
+        console.log(topicObj);
+        console.log(result);
+        setStatus(result.status);
+    })
+
+
   }
 
 
@@ -49,7 +66,7 @@ function Home() {
             <Card.Body>
               <Card.Title>Topic</Card.Title>
               <Form>
-                <Form.Control className="mb-3" type="text" placeholder="Enter a phrase" />
+                <Form.Control className="mb-3" type="text" placeholder="Enter a phrase" value={topic} onChange={handleOnChange}/>
                 <Button variant="primary w-100" onClick={handleSubmit} style={{backgroundColor: "#0E4456"}}>
                   Submit
                 </Button>
