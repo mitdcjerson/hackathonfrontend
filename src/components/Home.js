581dd3d255b6ff2411ef0179d60b17bb9276d8d7
Rica Janine Calcetas <calceri@mliph.manulife.com.ph>
2022-09-22 15:12:09 +0800

    home ui

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,42 @@
-import { Card, Container } from 'react-bootstrap';
+import { Card, Col, Container, Row } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import check from "../assets/verified.svg";
 
 function Home() {
   return (
+    <>
     <Container>
-      <Card>
-      <Card.Body>
-        <Card.Title>Topic</Card.Title>
-        <Form>
-          <Form.Control className="mb-3" type="text" placeholder="Running 5km" />
-          <Button variant="primary w-100" type="submit">
-            Submit
-          </Button>
-        </Form>
-      </Card.Body>
+      <Card className="mt-5">
+        <Card.Body>
+          <Card.Title>Topic</Card.Title>
+          <Form>
+            <Form.Control className="mb-3" type="text" placeholder="Running 5km" />
+            <Button variant="primary w-100" type="submit" style={{backgroundColor: "#0E4456"}}>
+              Submit
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>  
         
-      </Card>
     </Container>
+    <Container className="d-none">
+    
+      <Card className="mt-5">
+        <Card.Body>
+        <Row>
+          <Col>
+            <img src={check} style={{width: "100%", borderRadius: "50%"}}/>
+          </Col>
+          <Col className='my-auto'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          </Col>
+          </Row>
+        </Card.Body>
+      </Card>
+    
+  </Container>
+  </>
   );
 }
 
