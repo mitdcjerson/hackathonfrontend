bc93b920b5354d4a2357a24685d054227e02de6d
JunickMarco <workjunick@gmail.com>
2022-09-22 15:22:26 +0800

    Working navigation links

--- src/App.js
+++ src/App.js
@@ -2,16 +2,37 @@ import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Login from './components/Login';
+import Contribute from './components/Contribute';
 import Navigation from './components/Navigation';
 import Home from './components/Home';
+import { useState } from 'react';
+import { render } from '@testing-library/react';
 
 
 function App() {
+
+  const [page, setPage] = useState("home");
+
+  const renderPage = (page) => {
+    
+    if (page == "home") {
+      return <Home />
+    }
+    else if (page == "contribute") {
+      return <Contribute />
+    }
+    else if (page == "login") {
+      return <Login />
+    }
+
+  };
+
   return (
     <>
-    <Navigation/>
-    <Home/>
-    <Login/>
+    <Navigation setPage={setPage}/>
+      {
+        renderPage(page)
+      }
     </>
     // <div className="App">
     //   <header className="App-header">
