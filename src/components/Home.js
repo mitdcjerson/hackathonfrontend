ef86cde6ccbdce471ba64de832722b188f0c6efc
Rica Janine Calcetas <calceri@mliph.manulife.com.ph>
2022-09-22 16:09:54 +0800

    home result ui

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import { Card, Col, Container, Row } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import check from "../assets/verified.svg";
+import nodata from "../assets/no-data.svg";
 
 function Home() {
   const [ isFake, setIsFake] = useState();
@@ -13,34 +14,67 @@ function Home() {
   return (
     <>
     <Container>
-      <Card className="mt-5">
-        <Card.Body>
-          <Card.Title>Topic</Card.Title>
-          <Form>
-            <Form.Control className="mb-3" type="text" placeholder="Running 5km" />
-            <Button variant="primary w-100" type="submit" onClick={handleSubmit} style={{backgroundColor: "#0E4456"}}>
-              Submit
-            </Button>
-          </Form>
-        </Card.Body>
-      </Card>  
+      <Row>
+        <Col md={{ span: 6, offset: 3 }}>
+          <Card className="mt-5">
+            <Card.Body>
+              <Card.Title>Topic</Card.Title>
+              <Form>
+                <Form.Control className="mb-3" type="text" placeholder="Running 5km" />
+                <Button variant="primary w-100" type="submit" onClick={handleSubmit} style={{backgroundColor: "#0E4456"}}>
+                  Submit
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>  
+        </Col>
+      </Row>
+      
         
     </Container>
-    {/* <Container className={ isFake ? "d-none" : ""}> */}
-    <Container className="d-none">
-    
-      <Card className="mt-5">
+    <Container>
+    <Row>
+        <Col md={{ span: 6, offset: 3 }}>
+    {/* <Container className="d-none"> */}
+      {
+        isFake ?
+        <Card className="mt-5">
         <Card.Body>
         <Row>
           <Col>
-            <img src={check} style={{width: "100%", borderRadius: "50%"}}/>
+            <img src={check} style={{width: "100%"}}/>
           </Col>
           <Col className='my-auto'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+            <Card.Title>Its True</Card.Title>
+            <Card.Text muted>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
           </Col>
           </Row>
         </Card.Body>
       </Card>
+      :
+      <Card className="mt-5">
+        <Card.Body>
+        <Row>
+          <Col>
+            <img src={nodata} style={{width: "100%"}}/>
+          </Col>
+          <Col className='my-auto'>
+          <Card.Title>Its Fake</Card.Title>
+          <Card.Text muted>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          </Col>
+          </Row>
+        </Card.Body>
+      </Card>
+      }
+      </Col>
+      </Row>
+      
     
   </Container>
   </>
