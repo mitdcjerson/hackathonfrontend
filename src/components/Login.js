c5a02d111a32b86ff494b0cfe067520ba7ca6825
feredithmaye <feredithmaye@gmail.com>
2022-09-23 08:36:02 +0800

    Fix login request and response

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,56 +1,65 @@
-import { Alert, Button, Card, Col, Container, Form, Row, Toast } from "react-bootstrap";
+import { Alert, Button, Card, Col, Container, Form, Row } from "react-bootstrap";
 import { useState } from "react";
 import { Navigate } from 'react-router-dom';
+import '../App.css';
 
 function Login(props) {
 
-    const [uName, setUsername] = useState([]);
-    const [pass, setPassword] = useState([]);
+    const [uName, setUsername] = useState("");
+    const [pass, setPassword] = useState("");
     const [isError, setError] = useState();
-    
-    
-    function handleOnSubmit(event){
-        event.preventDefault();
 
+    function validateForm(){
+        return uName.length > 0 && pass.length > 0;
+    };
+
+    async function loginUser(credentials) {
         const userData = {
             username: uName,
             password: pass
         };
         console.log("User login", userData);
 
-        fetch("http://localhost:3002/users", {
+        return fetch("http://localhost:3002/users", {
             method: "POST",
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(userData)
-        }).then((data) => {
-            console.log("Login ", userData);
-
-            if (data === 0) {
-                setError(true);
-                props.setIsLoggedIn(false);
-            } else if (data === 1) {
-                //setError(false);
-                props.setIsLoggedIn(true);
-            }
-            
-            
         })
+        .then(data => data.json())
+    }
 
-        props.setIsLoggedIn(true);  //comment later to test with api
+    
+    async function handleOnSubmit(event){
+        event.preventDefault();
 
-        
+        const response = await loginUser({uName, pass});
+        console.log("response ", response);
+
+        if (response === 1){
+            console.log("success");
+            setError(false);
+            props.setIsLoggedIn(true);
+
+        } else if (response === 0){
+            console.log("failed");
+            setError(true);
+            props.setIsLoggedIn(false);
+        }
+
     };
 
     return(
         <div className="form-container">
             {
                 props.isLoggedIn ? <Navigate to="/contribute" replace /> : ""
-            }  
-            {/* {
-                isError ? <Toast>Login Error!</Toast> : <Toast>Login Success!</Toast>
-            } */}
+            }
+            {
+                isError ? <Alert className="mx-auto" variant="warning" onClose={() => setError(false)} dismissible>
+                    <p>Login failed. Please try again.</p>
+                </Alert> : ""
+            }
              
             <Container>
                 <Row>
@@ -79,7 +88,7 @@ function Login(props) {
                                     onChange={(e) => setPassword(e.target.value)}/>
                                 </Form.Group>
                                 
-                                <Button variant="primary w-100" type="submit" style={{backgroundColor: "#0E4456"}}>Sign In</Button>
+                                <Button variant="primary w-100" type="submit" style={{backgroundColor: "#0E4456"}} disabled={!validateForm()}>Sign In</Button>
                             </Form>
                         </Card.Body>
                     </Card>
